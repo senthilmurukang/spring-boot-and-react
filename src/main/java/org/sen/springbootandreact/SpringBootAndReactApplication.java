9ffac68972c218178ec18cc0135fdf7acd0cc60f
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2021-12-29 20:05:54 +0530

    initialized Spring Boot project

---
+++ src/main/java/org/sen/springbootandreact/SpringBootAndReactApplication.java
@@ -0,0 +1,13 @@
+package org.sen.springbootandreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndReactApplication.class, args);
+	}
+
+}
